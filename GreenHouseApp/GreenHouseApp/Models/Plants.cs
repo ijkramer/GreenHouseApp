363545631237fefeeb1a0ce682b6f33d19dc4464
kramerij <kramerisabelle09@gmail.com>
2024-07-22 17:45:57 -0400

    Initial commit

---
+++ GreenHouseApp/GreenHouseApp/Models/Plants.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenHouseApp.Models
+{
+    public class Plants
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public int WaterDays { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        public bool Notify { get; set; }
+    }
+
+}
+}
---
+++ GreenHouseApp/GreenHouseApp/ViewModels/AddPlant.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GreenHouseApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddPlant : ContentPage
+    {
+        public AddPlant()
+        {
+            InitializeComponent();
+        }
+    }
+}
